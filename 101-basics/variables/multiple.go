b3f41450bbce35b6109b9aa3bd7908dd0470929b
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-19 17:57:56 +0300

    Create file:  101-basics/variables/multiple.go
    -Add multiple declare variables

---
+++ 101-basics/variables/multiple.go
@@ -0,0 +1,19 @@
+package main
+
+var (
+	isItTrue  bool
+	isItFalse bool
+)
+
+const (
+	true = false
+)
+
+func main() {
+
+	isItTrue = true
+	isItFalse = false
+
+	println(isItTrue, isItTrue)
+
+}
